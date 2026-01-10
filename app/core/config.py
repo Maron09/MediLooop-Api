86b1db309d38bf35c80cab4760703b1b798f1f57
maron09 <chimarokeonyebi@gmail.com>
2026-01-10 06:19:52 +0100

    Async postgres, Alembic wired correctely, Future-proo migration

---
+++ app/core/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+
+
+class Settings(BaseSettings):
+    """Application configuration settings."""
+    PROJECT_NAME: str = Field(..., env='PROJECT_NAME')
+    API_V1_STR: str = Field(..., env='API_V1_STR')
+    
+    DATABASE_URL: str = Field(..., env='DATABASE_URL')
+    REDIS_URL: str = Field(..., env='REDIS_URL')
+    
+    JWT_SECRET_KEY: str = Field(..., env='JWT_SECRET_KEY')
+    JWT_ALGORITHM: str = Field(..., env='JWT_ALGORITHM')
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(..., env='ACCESS_TOKEN_EXPIRE_MINUTES')
+    
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+settings = Settings()
---
+++ app/db/sessions.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from app.core.config import settings
+
+
+engine = create_async_engine(settings.DATABASE_URL, echo=False, future=True)
+AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+
+async def get_db() -> AsyncSession: # type: ignore
+    """Get a database session."""
+    async with AsyncSessionLocal() as session:
+        yield session
